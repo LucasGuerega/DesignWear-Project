8d5ca875ba29bf5607da454fa86b0efe4ba88651
Lucas Guerega <lucas.g2006@aluno.ifsc.edu.br>
2024-05-15 17:10:36 -0300

    AjustesV9

--- assets/aulas/Aula-Saia/assets/js/click.js
+++ assets/aulas/Aula-Saia/assets/js/click.js
@@ -1,6 +1,17 @@
 function dicaFecho() {
-    document.getElementById("fechodica").src = "./assets/images/dicas/dicaFecho.png";
+    document.getElementById("dica").src = "./assets/images/dicas/dicaFecho.png";
+}
+function dicaComp() {
+    document.getElementById("dica").src = "./assets/images/dicas/dicaComprimento.png";
 }
 function volta() {
-    window.location.href='./index.html';
+    window.location.href = './index.html';
+}
+function voltP() {
+    document.getElementById("img").src = "./assets/images/material/CONTEUDOPinca/pinca.gif";
+    document.getElementById("legenda").src = "./assets/images/legendas/pinca.png";
+}
+function proxP() {
+    document.getElementById("img").src = "./assets/images/material/CONTEUDOPinca/pincavolumeok.gif";
+    document.getElementById("legenda").src = "./assets/images/legendas/pinca2.png";
 }
