ed8a07965409e2cf7121936bc9f2352b0d5b7fec
Lucas Guerega <lucas.g2006@aluno.ifsc.edu.br>
2024-05-22 17:04:48 -0300

    AjustesV11.1

--- assets/aulas/Aula-Saia/assets/js/index.js
+++ assets/aulas/Aula-Saia/assets/js/index.js
@@ -1,37 +1,25 @@
-const btnCintura = document.querySelector("#btn1");
-const btnFecho = document.querySelector("#btn2");
-const btnPinca = document.querySelector("#btn3");
-const btnComprimento = document.querySelector("#btn4");
-const btnAmplitude = document.querySelector("#btn5");
-const btnBainha = document.querySelector("#btn6");
-const Inspirar = document.querySelector("#inspi");
-
-btnCintura.addEventListener('click', () => {
-    window.location.href = './cintura.html';
-});
-
-btnFecho.addEventListener('click', () => {
-    window.location.href = './fecho.html';
-});
-
-btnPinca.addEventListener('click', () => {
-    window.location.href = './pinca.html';
-});
-
-btnComprimento.addEventListener('click', () => {
-    window.location.href = './comprimento.html';
-});
-
-btnAmplitude.addEventListener('click', () => {
-    window.location.href = './amplitude.html';
-});
-
-btnBainha.addEventListener('click', () => {
-    window.location.href = './bainha.html';
-});
-
-document.querySelector("#inspi").addEventListener('click', () => {
-    window.location.href = './inspirar.html';
-});
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    const btnCintura = document.querySelector("#btn1");
+    const btnFecho = document.querySelector("#btn2");
+    const btnPinca = document.querySelector("#btn3");
+    const btnComprimento = document.querySelector("#btn4");
+    const btnAmplitude = document.querySelector("#btn5");
+    const btnBainha = document.querySelector("#btn6");
+    const inspirar = document.querySelector("#inspire-se");
+  
+    // Função para adicionar ouvintes de clique
+    const addClickListener = (element, url) => {
+      element.addEventListener('click', () => {
+        window.location.href = url;
+      });
+    };
+  
+    addClickListener(btnCintura, './cintura.html');
+    addClickListener(btnFecho, './fecho.html');
+    addClickListener(btnPinca, './pinca.html');
+    addClickListener(btnComprimento, './comprimento.html');
+    addClickListener(btnAmplitude, './amplitude.html');
+    addClickListener(btnBainha, './bainha.html');
+    addClickListener(inspirar, './inspirar.html');
+  });
+  
